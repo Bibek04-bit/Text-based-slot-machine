bad4cc14e85baf64512179d3b311123d6f79bf6e
Bibek Acharya <92309375+Bibek04-bit@users.noreply.github.com>
2025-07-21 11:50:06 +0545

    updated

---
+++ main.py
@@ -0,0 +1,15 @@
+def deposit():
+    while True: 
+        amount = input("What amount would you like to deposit? Rs")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount>0:
+                break
+            else:
+                print("Ta pagal ho?")
+        else:
+            print("Please enter a number.")
+
+    return amount
+
+deposit()
